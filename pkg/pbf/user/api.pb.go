8cb8bff541fbdf6b263d8e34ef53fffda85e4ede
xh3b4sd <xh3b4sd@users.noreply.github.com>
2020-10-11 14:46:21 +0000

    update generated code

--- pkg/pbf/user/api.pb.go
+++ pkg/pbf/user/api.pb.go
@@ -7,7 +7,6 @@
 package api
 
 import (
-	proto "github.com/golang/protobuf/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -19,10 +18,6 @@ const (
 	// Verify that runtime/protoimpl is sufficiently up-to-date.
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
-
-// This is a compile-time assertion that a sufficiently up-to-date version
-// of the legacy proto package is being used.
-const _ = proto.ProtoPackageIsVersion4
 
 var File_api_proto protoreflect.FileDescriptor
 
